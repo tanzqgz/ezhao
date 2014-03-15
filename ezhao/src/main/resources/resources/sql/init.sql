4dfc1e9825beb63205d7d7d473934067fe4d7f03
tanzqgz <allentanzqgz@gmail.com>
2014-03-15 11:42:08 +0800

    update init.sql

--- ezhao/src/main/resources/resources/sql/init.sql
+++ ezhao/src/main/resources/resources/sql/init.sql
@@ -11,3 +11,22 @@ ENGINE = InnoDB
 COMMENT = '用户登录表';
 
 insert into `ezhao`.user(loginName,password) values('admin','admin');
+
+DROP TABLE IF EXISTS `ezhao`.`product`;
+CREATE TABLE `ezhao`.`product` (
+  `id` int(11) DEFAULT NULL,
+  `product_name` varchar(1000) DEFAULT NULL,
+  `description` blob,
+  `product_category` int(11) DEFAULT NULL,
+  `thumbnails` varchar(1000) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `ezhao`.`product_category`;
+CREATE TABLE `ezhao`.`product_category` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(1000) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `category_parent` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
